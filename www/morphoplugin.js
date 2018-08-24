20c1d859ce6ff56426f26c94fe97ed9b5231cebc
jcarraca <jcarraca@deloitte.pt>
2018-08-24 15:15:24 -0500

    changes to www/morphoplugin.js

--- www/morphoplugin.js
+++ www/morphoplugin.js
@@ -22,48 +22,12 @@ var serial = {
             [{'opts': opts}]
         );
     },
-    write: function(data, successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'Serial',
-            'writeSerial',
-            [{'data': data}]
-        );
-    },
-    writeHex: function(hexString, successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'Serial',
-            'writeSerialHex',
-            [{'data': hexString}]
-        );
-    },
-    read: function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'Serial',
-            'readSerial',
-            []
-        );
-    },
     close: function(successCallback, errorCallback) {
         cordova.exec(
             successCallback,
             errorCallback,
             'Serial',
             'closeSerial',
-            []
-        );
-    },
-    registerReadCallback: function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'Serial',
-            'registerReadCallback',
             []
         );
     }
